bf33f734c4fd60959f0e3639712c64ad23a00525
omidfa <omidfa1234@gmail.com>
2022-09-13 01:50:19 +0430

    working on google login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,11 @@
 import NextAuth from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
-import CredentialsProvider from 'next-auth/providers/credentials';
 import EmailProvider from 'next-auth/providers/email';
-import dbConnect from 'utils/dbConnect';
-import { findUser } from 'server/services/user.services';
-import { passwordCompare } from 'utils/tools';
 import { NextApiRequest, NextApiResponse } from 'next';
-import type { NextAuthOptions } from 'next-auth';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from 'utils/mongoClient';
 import GoogleProvider from 'next-auth/providers/google';
+import nextAuth from 'next-auth';
+import type { NextAuthOptions } from 'next-auth';
 
 export const authOptions: NextAuthOptions = {
   adapter: MongoDBAdapter(clientPromise),
@@ -24,6 +20,7 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.JWT_SECRET,
+
   // callbacks: {
   //   async jwt({ token, user }) {
   //     if (user?._id) token._id = user._id;
@@ -36,8 +33,7 @@ export const authOptions: NextAuthOptions = {
   //     return session;
   //   },
   // },
+  debug: true,
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  NextAuth(req, res, authOptions);
-}
+export default nextAuth(authOptions);
